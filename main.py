b5d06d1546ae3bc72348b794eb45787a6f85f69b
Venia <veniamin.popidoha@gmail.com>
2022-02-26 10:33:27 +0200

    1

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+# -*- coding: utf-8 -*-
 import telebot
-owner = 189898376
+owner = -1001501167582
 from telebot import types
-token='1521374248:AAH_KjkeC-9r6NW0tdLXGoo1gOfo8iNxSkA'
+token='5100248003:AAGBhr-bp70CLhYq6c3yG2Mo0u-PXZqgNcM'
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id,'Я готовий вислухати вас...')
+	bot.send_message(message.chat.id, 'Я готовий вислухати вас...')
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
         
   if(message.chat.id == owner):
-    bot.send_message(message.chat.id, 'Ви є власником) Я працюю на всі 600') 
+    bot.send_message(message.chat.id, 'Ви є власником)')
   else:
     bot.forward_message(owner, message.chat.id, message.message_id)
     bot.send_message(message.chat.id, message.from_user.first_name + ', Ваше запитання вже в роботі, очікуйте відповіді:)')
